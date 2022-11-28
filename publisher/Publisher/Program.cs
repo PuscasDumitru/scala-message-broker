03b0afa289a101979a1418c7c44c51b56e2ccfd9
Dumitru Puscas <7x1yy813nx19@gmail.com>
2022-11-29 00:02:15 +0200

    Refactor publisher

--- publisher/Publisher/Program.cs
+++ publisher/Publisher/Program.cs
@@ -9,80 +9,62 @@ namespace Publisher
 {
     public class Program
     {
+
         static async Task Main(string[] args)
         {
-            var client = new HttpClient();
+            var tweetsUrl = "http://localhost:8080/tweets/1";
 
-            var url = "http://localhost:8080/tweets/1";
+            var clientToGetTweets = new HttpClient();
+            TcpClient clientForBroker;
 
-            //var payload = new StringContent("something", Encoding.UTF8, "application/json");
-            //var result = client.PostAsync(endpoint, payload).Result;
-
-            //TcpClient client;
-            //BinaryWriter writer;
-            //BinaryReader reader;
-            //NetworkStream stream;
-
-            //while (true)
-            //{
-
+            BinaryWriter writer;
+            BinaryReader reader;
+            NetworkStream stream;
 
             try
             {
+                Console.WriteLine("Establishing connection with tweets stream...");
 
-                Console.WriteLine("Establishing connection...");
-
-                var list = new List<object>();
-
-                using (var streamReader = new StreamReader(await client.GetStreamAsync(url)))
+                using (var streamReader = new StreamReader(await clientToGetTweets.GetStreamAsync(tweetsUrl)))
                 {
-                    var num = 0;
-
                     while (!streamReader.EndOfStream)
                     {
-                        if (num > 1000)
-                            break;
-
                         var message = await streamReader.ReadLineAsync();
 
                         if (message is not null && message.Contains("data:"))
                         {
-                            list.Add(message);
-                            num++;
+                            var input = Console.ReadLine();
+                            var splitCommands = input.Split(" ");
+                            
+                            var serializedMessage = JsonConvert.SerializeObject(
+                                new
+                                {
+                                    topic = splitCommands[1],
+                                    message
+                                });
+
+                            using (clientForBroker = new TcpClient("127.0.0.1", 8081))
+                            {
+                                Console.WriteLine("Connection with broker established...");
+                                using (stream = clientForBroker.GetStream())
+                                {
+                                    Console.WriteLine("Stream retrieved from the connection...");
+
+                                    //string received = null;
+                                    writer = new BinaryWriter(stream);
+                                    writer.Write(serializedMessage);
+                                    Console.WriteLine("Message sent successfully...\n\n");
+                                    //reader = new BinaryReader(stream);
+                                    //received = reader.ReadString();
+                                    //Console.WriteLine(received + " was received...");
+                                }
+                            }
+
                         }
-                        //streamReader.
-                        
-                        //var deserMessage = JsonConvert.DeserializeObject(message);
-                        //    Console.WriteLine(message);
-                        //    list.Add(message);
+
                     }
-
-                    Console.WriteLine("There are: " + list.Count + " RECORDS");
                 }
-
-                //using (client = new TcpClient("127.0.0.1", 8080))
-                //{
-                //    Console.WriteLine("Connection established...");
-                //    using (stream = client.GetStream())
-                //    {
-                //        Console.WriteLine("Stream retrieved from the connection...");
-                //        var sent1 = new 
-                //        {
-                //            Name = "name",
-                //            Id = "id"
-                //        };
-
-                //        var sent = JsonConvert.SerializeObject(sent1);
-
-                //        string received = null;
-                //        writer = new BinaryWriter(stream);
-                //        writer.Write(sent);
-                //        Console.WriteLine(sent + " was sent...");
-                //        reader = new BinaryReader(stream);
-                //        received = reader.ReadString();
-                //        Console.WriteLine(received + " was received...");
-                //    }
-                //}
+                
             }
             catch (Exception e)
             {
